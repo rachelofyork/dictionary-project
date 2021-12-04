7e0f23b2d187450be7fdf65da4e0b5bd61ced3c8
rachelofyork <rachelofyork@gmail.com>
2021-12-04 14:41:49 +0000

    Integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react"
+import axios from "axios";
 import "./Dictionary.css"
 
 export default function Dictionary(){
 const [keyword, setKeyword] = useState("");
 
+function handleResponse(response){
+    console.log(response.data[0])
+}
+
 function search(event) {
     event.preventDefault();
-    alert (`Searching for definition of ${keyword}...`)
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+    
 }
+// https://dictionaryapi.dev/
 
 function handleKeywordChange(event){
     event.preventDefault()
@@ -18,7 +26,7 @@ function handleKeywordChange(event){
     return <div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" placeholder="Search for a word..." onChange={handleKeywordChange}/>
-            <input type="submit" vaue="Search" />
+            <input type="submit" value="Search" />
         </form>
     </div>
 }
